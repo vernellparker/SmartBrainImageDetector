3ace2c717d774d5f9241449fe7970b593d3df520
vernellparker <vernell.parker@gmail.com>
2019-07-07 03:12:36 -0400

    Init commit app shell

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+ const Navigation = ({onRouteChange,isSignedIn}) => {
+        if(isSignedIn){
+            return(
+             <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                 <p onClick={() => onRouteChange('signOut')} className={'f3 link black underline pa3 pointer'}>Sign Out</p>
+             </nav>
+            )
+        } else {
+            return(
+                <div>
+                    <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                        <p onClick={() => onRouteChange('signIn')} className={'f3 link black underline pa3 pointer'}>Sign In</p>
+                        <p onClick={() => onRouteChange('register')} className={'f3 link black underline pa3 pointer'}>Register</p>
+                    </nav>
+                </div>
+            )
+    }
+};
+export default Navigation;
